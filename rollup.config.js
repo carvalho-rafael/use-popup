828d122d65baeed81f1f63c923d32fd9b2c7f964
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-05-05 22:42:05 -0300

    use-popup fist version

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from 'rollup-plugin-typescript2'
+import css from 'rollup-plugin-import-css'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false
+    }
+  ],
+  plugins: [css(), typescript()],
+  external: ['react', 'react-dom']
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { useMemo, useState } from "react";
+import './popup.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export type PopupProps = {
+    title: string,
+    message: string,
+    color: string,
+    seconds: number
+}
+
+export default function usePopup() {
+    const [active, setActive] = useState(false)
+    const [title, setTitle] = useState('')
+    const [message, setMessage] = useState('')
+    const [color, setColor] = useState('')
+
+    function showPopup({ title, message, color, seconds }: PopupProps) {
+        setTitle(title);
+        setMessage(message);
+        setColor(color);
+
+        setActive(true)
+
+        setTimeout(() => {
+            setActive(false)
+        }, seconds * 1000);
+    }
+
+    const Popup = useMemo(() =>
+        () => active ? (
+            <aside className="popup-container" style={{background: color}}>
+                <h3>{title}</h3>
+                <p>{message}</p>
+            </aside>
+        ) : <> </>
+        , [active])
+
+    return { Popup, showPopup };
+}
